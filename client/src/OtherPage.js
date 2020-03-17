b32a4e320e18b2b669e2ca62b2a9a5ba74026ea3
AmirArreaza <ajaarreazav@gmail.com>
2020-03-17 15:13:10 +0000

    Initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+    return (
+        <div>
+            En otra paginish!
+            <Link to="/"> Pa atras menor</Link>
+        </div>
+    );
+};
